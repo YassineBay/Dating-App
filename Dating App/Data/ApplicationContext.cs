acaa395603f9ef377857906a9b1942c63562fe14
YassineBay <yassine.bayoudh@esprit.tn>
2020-12-23 15:27:38 +0100

    Add project files.

---
+++ Dating App/Data/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Dating_App.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dating_App.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options):base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
